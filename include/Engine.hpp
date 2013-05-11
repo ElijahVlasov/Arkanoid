fea6ef28a8d9ea7fd87d64d4498062c8a57cb272
Elijah Vlasov <elijah.vlasov@yandex.ru>
2013-05-11 17:56:40 +0400

    Engine.hpp

---
+++ include/Engine.hpp
@@ -0,0 +1,13 @@
+#ifndef _ENGINE_HPP
+#define _ENGINE_HPP
+
+#include <Engine\Controller.hpp>
+#include <Engine\Direction.hpp>
+#include <Engine\Engine.hpp>
+#include <Engine\Object.hpp>
+#include <Engine\Player.hpp>
+#include <Engine\Renderer.hpp>
+#include <Engine\World.hpp>
+
+
+#endif
