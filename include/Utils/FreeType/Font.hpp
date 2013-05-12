02fd62134557bc05077046a779697b9d60826048
Elijah Vlasov <elijah.vlasov@yandex.ru>
2013-05-12 20:31:28 +0400

    Markup changed

--- include/Utils/FreeType/Font.hpp
+++ include/Utils/FreeType/Font.hpp
@@ -19,46 +19,46 @@
 
 namespace Utils {
 
-	namespace FreeType {
+    namespace FreeType {
 
-		class Font {
+        class Font {
 	
-			public:
+            public:
 
-				typedef boost::array<uint8_t, 3> Color;
+                typedef boost::array<uint8_t, 3> Color;
 
-				explicit Font(const char* filePath) throw(std::runtime_error, std::invalid_argument);
-				explicit Font(const std::string& filePath) throw(std::runtime_error, std::invalid_argument);
-				Font(const Font& font);
+                explicit Font(const char* filePath)         throw(std::runtime_error, std::invalid_argument);
+                explicit Font(const std::string& filePath)  throw(std::runtime_error, std::invalid_argument);
+                Font(const Font& font);
 
-				~Font();
+                ~Font();
 
-				Font& operator=(const Font& font);
+                Font& operator=(const Font& font);
 
-				// генерирует invalid_argument, если text == 0.
-				// runtime_error, если 
-				Texture renderText(const char* text) throw(std::invalid_argument, std::runtime_error);
-				Texture renderText(const std::string& text) throw(std::runtime_error);
+                // генерирует invalid_argument, если text == 0.
+                // runtime_error, если 
+                Texture renderText(const char* text)        throw(std::invalid_argument, std::runtime_error);
+                Texture renderText(const std::string& text) throw(std::runtime_error);
 
-				unsigned int getSize() const;
-				void setSize(unsigned int size) throw(std::invalid_argument);
+                unsigned int getSize() const;
+                void setSize(unsigned int size)             throw(std::invalid_argument);
 
-				Color getColor() const;
-				void setColor(const Color& color);
+                Color getColor() const;
+                void setColor(const Color& color);
 
-			private:
+            private:
 
-				Library* lib_;
+                Library* lib_;
 				
-				boost::shared_ptr<FT_Face> facePtr_;
+                boost::shared_ptr<FT_Face> facePtr_;
 
-				Color color_;
+                Color color_;
 
-				unsigned int size_;
+                unsigned int size_;
 
-		};
+        };
 
-	}
+    }
 
 }
 
