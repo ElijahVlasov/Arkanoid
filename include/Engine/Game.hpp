b327ee823a800944e53f41aa1a5d3d96db8ab08d
Elijah Vlasov <elijah.vlasov@yandex.ru>
2013-05-12 21:13:06 +0400

    Markup changed

--- include/Engine/Game.hpp
+++ include/Engine/Game.hpp
@@ -1,6 +1,6 @@
 /*****************************************************************
  
-				 Главный класс приложения. 
+                Главный класс приложения. 
 
 
 *****************************************************************/
@@ -18,46 +18,46 @@
 
 namespace Engine {
 	
-	class Game: public Utils::Singleton<Game> {
+    class Game: public Utils::Singleton<Game> {
 
-		SINGLETON(Game)
+        SINGLETON(Game)
 
-		public:
+        public:
 	
-			void onRender();
+            void onRender();
 
-			void onKeyDown(int key);
-			void onKeyUp(int key);
+            void onKeyDown(int key);
+            void onKeyUp(int key);
 
-			void onMouseMotion(int x, int y);
-			void onMouseDown(int x, int y, Utils::MouseButton mouseButton);
-			void onMouseUp(int x, int y, Utils::MouseButton mouseButton);
+            void onMouseMotion(int x, int y);
+            void onMouseDown(int x, int y, Utils::MouseButton mouseButton);
+            void onMouseUp(int x, int y, Utils::MouseButton mouseButton);
 
-			void setScreenRect(unsigned int width, unsigned int height);
+            void setScreenRect(unsigned int width, unsigned int height);
 
-			int getScreenWidth() const;
-			int getScreenHeight() const;
+            int getScreenWidth() const;
+            int getScreenHeight() const;
 
-			const GameStates::IGameState* getState() const;
-			void setState(GameStates::IGameState* state);
+            const GameStates::IGameState* getState() const;
+            void setState(GameStates::IGameState* state);
 
-			void setRunning(bool isRunning);
-			bool isRunning() const;
+            void setRunning(bool isRunning);
+            bool isRunning() const;
 
-		protected:
+        protected:
 
-			Game();
-			~Game();
+            Game();
+            ~Game();
 
-		private:
+        private:
 
-			bool isRunning_;
+            bool isRunning_;
 
-			GameStates::IGameState* state_;
+            GameStates::IGameState* state_;
 
-			unsigned int scrWidth_, scrHeight_;
+            unsigned int scrWidth_, scrHeight_;
 
-	};
+    };
 
 }
 
