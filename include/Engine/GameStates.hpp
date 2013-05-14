bba7b1c1d0b1add38f670d728aea6da882e19e24
Elijah Vlasov <elijah.vlasov@yandex.ru>
2013-05-14 17:55:51 +0400

    Game state classes

---
+++ include/Engine/GameStates.hpp
@@ -0,0 +1,8 @@
+#ifndef _SALT2D_ENGINE_GAMESTATES_HPP
+#define _SALT2D_ENGINE_GAMESTATES_HPP
+
+#include <Engine/GameStates/IGameState.hpp>
+#include <Engine/GameStates/MenuState.hpp>
+#include <Engine/GameStates/SingleGameState.hpp>
+
+#endif
