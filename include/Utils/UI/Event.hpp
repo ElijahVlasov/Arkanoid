51ee5f8cc3c4a670193ac5ade14ffe63cd28c826
Elijah Vlasov <elijah.vlasov@yandex.ru>
2013-05-19 11:10:21 +0400

    Doxygen documentation added

--- include/Utils/UI/Event.hpp
+++ include/Utils/UI/Event.hpp
@@ -1,10 +1,14 @@
-#ifndef _SALT2D_UTILS_UI_EVENT_HPP
+﻿#ifndef _SALT2D_UTILS_UI_EVENT_HPP
 #define _SALT2D_UTILS_UI_EVENT_HPP
 
 namespace Utils {
 
 	namespace UI {
 	
+        /** Событие UI.
+          * @author Elijah Vlasov
+        */
+    
 		class Event {};
 
 	}
