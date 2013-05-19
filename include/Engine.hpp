5f13d6a8622eec4589732cad7c7e4fdf5b7ca788
Elijah Vlasov <elijah.vlasov@yandex.ru>
2013-05-19 12:03:15 +0400

    Doxygen documentation added

--- include/Engine.hpp
+++ include/Engine.hpp
@@ -1,13 +1,28 @@
-#ifndef _ENGINE_HPP
+﻿#ifndef _ENGINE_HPP
 #define _ENGINE_HPP
 
-#include <Engine\Controller.hpp>
-#include <Engine\Direction.hpp>
-#include <Engine\Engine.hpp>
-#include <Engine\Object.hpp>
-#include <Engine\Player.hpp>
-#include <Engine\Renderer.hpp>
-#include <Engine\World.hpp>
+#include <Engine/Controller.hpp>
+#include <Engine/Direction.hpp>
+#include <Engine/Object.hpp>
+#include <Engine/Player.hpp>
+#include <Engine/Renderer.hpp>
+#include <Engine/SaltEngine.hpp>
+#include <Engine/World.hpp>
 
 
 #endif
+
+/** Движок игры.
+  * @author Elijah Vlasov
+*/
+
+namespace Engine {
+
+    class Controller;
+    class Object;
+    class Player;
+    class Renderer;
+    class SaltEngine;
+    class World;
+
+}
