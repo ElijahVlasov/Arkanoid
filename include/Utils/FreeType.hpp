4ba25593e0a6df2c2beb044c7a614dfe51c6ad82
unknown <Teacher@CL-528-1.itstep.local>
2013-05-19 10:11:50 +0400

    Documentation added

--- include/Utils/FreeType.hpp
+++ include/Utils/FreeType.hpp
@@ -1,7 +1,22 @@
-#ifndef _SALT2D_UTILS_FREETYPE_HPP
+﻿#ifndef _SALT2D_UTILS_FREETYPE_HPP
 #define _SALT2D_UTILS_FREETYPE_HPP
 
 #include <Utils/FreeType/Font.hpp>
 #include <Utils/FreeType/Library.hpp>
 
 #endif
+
+namespace Utils {
+
+    /** Обертка над FreeType.
+      * @author Elijah Vlasov
+    */
+
+    namespace FreeType {
+
+        class Font;
+        class Library;
+
+	}
+
+}
