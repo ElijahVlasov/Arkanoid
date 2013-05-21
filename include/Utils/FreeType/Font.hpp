34c5dd61cdf04e2a6b25f835effc0ea4e90aae99
Elijah Vlasov <elijah.vlasov@yandex.ru>
2013-05-21 09:14:45 +0400

    Doxygen documentation adde

--- include/Utils/FreeType/Font.hpp
+++ include/Utils/FreeType/Font.hpp
@@ -21,11 +21,31 @@ namespace Utils {
 
     namespace FreeType {
 
+        /** Класс шрифта.
+          * @author Elijah Vlasov
+        */
+
         class Font {
 
             public:
 
+                /** Загрузить шрифт из файла.
+                  * Шрифт ищется в папке FONT_PATH
+                  * @param filePath Имя файла шрифта.
+                  * @throws Генерирует std::invalid_argument, если filePath == NULL,
+                  *         или пустая строка. Генерирует std::runtime_error, если произошла
+                  *         ошибка загрузки.
+                */
+
                 explicit Font(const char* filePath)         throw(std::invalid_argument, std::runtime_error);
+
+                /** Загрузить шрифт из файла.
+                  * Шрифт ищется в папке FONT_PATH
+                  * @param filePath Имя файла шрифта.
+                  * @throws Генерирует std::invalid_argument, если filePath - пустая строка. 
+                  *         Генерирует std::runtime_error, если произошла ошибка загрузки.
+                */
+
                 explicit Font(const std::string& filePath)  throw(std::invalid_argument, std::runtime_error);
                 Font(const Font& font);
 
@@ -33,15 +53,40 @@ namespace Utils {
 
                 Font& operator=(const Font& font);
 
-                // генерирует invalid_argument, если text == 0.
-                // runtime_error, если 
+                /** Отрендерить текст в OpenGL-текстуру.
+                  * @param text Текст для рендеринга 
+                  * @throws Генерирует std::invalid_argument, если text == NULL.
+                  *         Генерирует std::runtime_error, при ошибке Freetype2
+                */
+
                 Texture renderText(const char* text)        throw(std::invalid_argument, std::runtime_error);
+
+                /** Отрендерить текст в OpenGL-текстуру.
+                  * @param text Текст для рендеринга 
+                  * @throws Генерирует std::runtime_error, при ошибке Freetype2
+                */
+
                 Texture renderText(const std::string& text) throw(std::runtime_error);
 
+                /** Кегль шрифта.
+                */
+
                 unsigned int getSize() const;
+
+                /** Установить кегль шрифта.
+                  * @throw Генерирует std::invalid_argument, если size == 0.
+                */
+
                 void setSize(unsigned int size)             throw(std::invalid_argument);
 
+                /** Цвет шрифта.
+                */
+
                 const Utils::Color& getColor() const;
+
+                /** Установить цвет шрифта.
+                */
+
                 void setColor(const Utils::Color& color);
 
             private:
