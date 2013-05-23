78f5c7f9214729d6230713e86ed9f6283ec9e02d
Elijah Vlasov <elijah.vlasov@yandex.ru>
2013-05-23 20:02:05 +0400

    PCResourceLoader added

---
+++ include/Utils/PCResourceLoader.hpp
@@ -0,0 +1,50 @@
+#ifndef _SALT2D_UTILS_PCRESOURCELOADER_HPP
+#define _SALT2D_UTILS_PCRESOURCELOADER_HPP
+
+#include <stdexcept>
+#include <string>
+
+#include <boost/shared_ptr.hpp>
+
+#include <IL/il.h>
+
+#include <Utils/Resource.hpp>
+#include <Utils/Singleton.hpp>
+#include <Utils/Texture.hpp>
+
+namespace Utils {
+
+    /** Загрузчик ресурсов для ПК.
+      * @author Elijah Vlasov
+    */
+
+    class PCResourceLoader: public Singleton<ResourceLoader> {
+
+        SINGLETON(ResourceLoader)
+
+        public:
+
+            boost::shared_ptr<Resource> loadResource(ResourceType resourceType, const char* resourceName)
+                                                        throw(std::invalid_argument, std::runtime_error);
+
+            boost::shared_ptr<Resource> loadResource(ResourceType resourceType, const std::string& resourceName)
+                                                        throw(std::invalid_argument, std::runtime_error);
+
+        protected:
+
+            PCResourceLoader();
+            ~PCResourceLoader();
+
+        private:
+
+            boost::shared_ptr<Resource> loadTexture(const char* fileName) throw(std::runtime_error);
+            boost::shared_ptr<Resource> loadPlainText(const char* fileName) throw(std::runtime_error);
+            boost::shared_ptr<Resource> loadBinaryFile(const char* fileName) throw(std::runtime_error);
+
+            void ILImageToTexture(ILuint ilTex, Texture* texture);
+
+    };
+
+}
+
+#endif
---
+++ src/Utils/PCResourceLoader.cpp
@@ -0,0 +1,222 @@
+#include <cstring>
+
+#include <ios>
+#include <fstream>
+#include <stdexcept>
+#include <string>
+
+#include <boost/format.hpp>
+#include <boost/shared_ptr.hpp>
+
+#include "config.h"
+
+#ifdef MS_WINDOWS
+#include <Windows.h>
+#endif
+
+#include <GL/gl.h>
+
+#include <IL/il.h>
+#include <IL/ilu.h>
+
+#include <Utils.hpp>
+
+#include <Utils/PCResourceLoader.hpp>
+
+using namespace std;
+
+using namespace Utils;
+
+
+
+PCResourceLoader::PCResourceLoader() {
+
+    ilInit();
+    iluInit();
+
+}
+
+
+
+PCResourceLoader::~PCResourceLoader() {}
+
+
+
+boost::shared_ptr<Resource> PCResourceLoader::loadResource(ResourceLoader::ResourceType resourceType, const char* resourceName) 
+                                                throw(invalid_argument, runtime_error) {
+
+    ASSERT(
+        (resourceName != 0),
+        invalid_argument("resourceName")
+    );
+
+    ASSERT(
+        (strlen(resourceName) != 0),
+        invalid_argument("resourceName")
+    );
+
+    switch(resourceType) {
+
+        case BINARY_FILE: {
+            return loadBinaryFile(resourceName);
+        }
+        break;
+
+        case PLAIN_TEXT: {
+            return loadPlainText(resourceName);
+        }
+        break;
+
+        case TEXTURE: {
+            return loadTexture(resourceName);
+        }
+        break;
+
+    }
+
+    return boost::shared_ptr();
+
+}
+
+
+
+boost::shared_ptr<Resource> PCResourceLoader::loadPlainText(const char* resourceName) throw(runtime_error) {
+
+    // Получаем строку вида ресурсы/имя_ресурса
+    string path = (boost::format("%1%/%2%")
+                      % RESOURCE_PATH % resourceName
+                  ).str();
+
+    ResourceFile* resource = new ResourceFile();
+
+    ifstream resourcetStream(path);
+
+    // Если ошибка потока
+    ASSERT(
+        resourceStream.good(),
+        runtime_error(
+            (boost::format("Can't find file \"%1%\"")
+                % path
+            ).str()
+        )
+    );
+
+    string data = Utils::readStreamToString(resourceStream);
+
+    resource->setData(data);
+
+    return boost::shared_ptr<Resource>(resource);
+
+}
+
+
+
+boost::shared_ptr<Resource> PCResourceLoader::loadBinaryFile(const char* resourceName) throw(runtime_error) {
+
+     // Получаем строку вида ресурсы/имя_ресурса
+    string path = (boost::format("%1%/%2%")
+                      % RESOURCE_PATH % resourceName
+                  ).str();
+
+    ResourceFile* resource = new ResourceFile();
+
+    ifstream resourceStream(path, ios::binary);
+
+    // Если ошибка потока
+    ASSERT(
+        resourceStream.good(),
+        runtime_error(
+            (boost::format("Can't find file \"%1%\"")
+                % path
+            ).str()
+        )
+    );
+
+    string data = Utils::readBinaryStream(resourceStream);
+
+    resource->setData(data);
+
+    return boost::shared_ptr<Resource>(resource); 
+
+}
+
+
+
+boost::shared_ptr<Resource> PCResourceLoader::loadTexture(const char* resourceName) throw(runtime_error) {
+
+    string path = (boost::format("%1%/%2%")
+                      % TEXTURE_PATH % fileName
+                  ).str();
+
+    boost::shared_ptr<Resource> resource = loadBinaryFile(resourceName);
+
+    Texture* texture = new Texture();
+
+    string textureData = resource.getData();
+
+    ILuint ilTexture;
+
+    ilGenImages(1, &ilTexture);
+
+    ilBindImage(ilTexture);
+
+    ASSERT(
+        ilLoadL(IL_PNG, textureData.data(), textureData.size()),
+        runtime_error(
+            (boost::format("Can't load \"%1%\":\n%2%")
+                % resourceName
+                % iluErrorString(ilGetError()))
+            ).str()
+        )
+    );
+
+    ILImageToTexture(ilTexture, texture);
+
+    return boost::shared_ptr<Resource>(texture);
+
+}
+
+
+
+void PCResourceLoader::ILImageToTexture(ILuint ilTex, Texture* texture) {
+
+    unsigned int width, height, bpp;
+
+    ilBindImage(texture);
+
+    width   =  ilGetInteger(IL_IMAGE_WIDTH);
+    height  =  ilGetInteger(IL_IMAGE_HEIGHT);
+
+    texture->setWidth(width);
+    texture->setHeight(height);
+
+    ILint format = ilGetInteger(IL_IMAGE_FORMAT);
+
+    switch(format) {
+
+        case IL_RGB: {
+            texture->setFormat(GL_RGB);
+            bpp = 3;
+        }
+        break;
+
+        case IL_RGBA: {
+            texture->setFormat(GL_RGBA);
+            bpp = 4;
+        }
+        break;
+
+    }
+
+    string textureData;
+
+    textureData.resize(width * height * bpp);
+
+    ilCopyPixels(0, 0, 0,
+                 width, height, 1,
+                 format, IL_UNSIGNED_BYTE, const_cast<ILvoid*>(textureData.data())
+                );
+
+    texture->setData(textureData);
+
+}
