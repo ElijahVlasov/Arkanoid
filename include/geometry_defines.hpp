9c7484e45a15062a119d2f970b9dc19efd1b679b
Elijah Vlasov <elijah_vlasov@yandex.ru>
2013-07-09 17:57:00 +0300

    float to int models cast added

--- include/geometry_defines.hpp
+++ include/geometry_defines.hpp
@@ -1,6 +1,8 @@
 #ifndef _SALT2D_GEOMERTY_DEFINES_HPP
 #define _SALT2D_GEOMERTY_DEFINES_HPP
 
+#include <boost/foreach.hpp>
+
 #include <boost/geometry/geometries/box.hpp>
 #include <boost/geometry/geometries/point_xy.hpp>
 #include <boost/geometry/geometries/polygon.hpp>
@@ -16,6 +18,43 @@ namespace GeometryDefines {
     typedef boost::geometry::model::d2::point_xy<float> Point;
     typedef boost::geometry::model::box<Point>          Box;
     typedef boost::geometry::model::polygon<Point>      Polygon;
+
+    inline Point PointIToPoint(const PointI&);
+    inline Box BoxIToBox(const BoxI&);
+    inline Polygon PolygonIToPolygon(const PolygonI&);
+
+}
+
+
+
+GeometryDefines::Point GeometryDefines::PointIToPoint(const GeometryDefines::PointI& point) {
+
+    return GeometryDefines::Point(static_cast<float>(point.x()), static_cast<float>(point.y()));
+
+}
+
+
+
+GeometryDefines::Box GeometryDefines::BoxIToBox(const GeometryDefines::BoxI& box) {
+
+    return GeometryDefines::Box(GeometryDefines::PointIToPoint(box.min_corner()), GeometryDefines::PointIToPoint(box.max_corner()));
+
+}
+
+
+
+GeometryDefines::Polygon GeometryDefines::PolygonIToPolygon(const GeometryDefines::PolygonI& polygon) {
+
+    GeometryDefines::Polygon res;
+    GeometryDefines::Polygon::ring_type& resOuter = res.outer();
+
+    BOOST_FOREACH(GeometryDefines::PointI point, polygon.outer()) {
+
+        resOuter.push_back(GeometryDefines::PointIToPoint(point));
+
+    }
+
+    return res;
 
 }
 
