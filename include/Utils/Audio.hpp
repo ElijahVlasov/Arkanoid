15088a479413a24253cb724487b53dc1934d3f13
Elijah Vlasov <elijah.vlasov@gmail.com>
2013-08-05 18:45:21 +0400

    added audio header

---
+++ include/Utils/Audio.hpp
@@ -0,0 +1,15 @@
+#ifndef _SALT2D_UTILS_AUDIO_HPP
+#define _SALT2D_UTILS_AUDIO_HPP
+
+#include <Utils/Audio/AudioManager.hpp>
+#include <Utils/Audio/Buffer.hpp>
+#include <Utils/Audio/IStream.hpp>
+#include <Utils/Audio/OGGSound.hpp>
+#include <Utils/Audio/OGGSoundBuilder.hpp>
+#include <Utils/Audio/OGGStream.hpp>
+#include <Utils/Audio/PCMSound.hpp>
+#include <Utils/Audio/PCMStream.hpp>
+#include <Utils/Audio/Sound.hpp>
+#include <Utils/Audio/SoundPlayer.hpp>
+
+#endif
